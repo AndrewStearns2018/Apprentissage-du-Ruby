bf5a0be7e82aac37b140f6da1fb202ea80507093
Thomas Stearns <TRS@Thomass-MacBook-Pro-2.local>
2017-07-20 11:24:25 -0400

    adds function comments

--- first_program.rb
+++ first_program.rb
@@ -23,3 +23,41 @@ puts (the_number.to_i * the_number.to_i)
 puts ("Tapez votre nom pour dire bonjour")
 nom = gets.chomp
 puts ("Je m'appelle " + nom + ". " + "Comment vous appelez-vous ?")
+
+"""
+thomas' comments:
+
+always try to avoid using 'global variables' in computer programs.
+these are variables that are set on left-most part of the screen, like
+
+x = 4
+
+'global' means that the entire program has access to that variable.
+Instead you should try to organize a program into individual functions
+(methods) than do a single simple task and can then be combined.
+
+For example, above you have
+
+integer_conversion = '5'.to_i + '6'.to_i
+
+this converts a pair of strings to integers. instead try this:
+"""
+
+def sum_strings_together(first_string, second_string)
+  first_string = first_string.to_i
+  second_string = second_string.to_i
+  sum = first_string.to_i + second_string.to_i
+
+  return sum
+end
+
+puts(sum_strings_together("5", "14"))
+puts(sum)
+
+"""
+what does this code do? it defines a function (with 'def') that takes a pair
+of strings as 'arguments' (first_string and second_string), coverts them to integers
+and then returns that variable.
+
+why does puts(sum) fail? because sum is a variable ONLY defined inside the function (it is not global)
+"""
