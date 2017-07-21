5824563bff51ff454e6986805dc31e77b6869e34
BarondeCharlus <andrew.stearns@sciencespo.fr>
2017-07-21 20:11:21 +0200

    Add files via upload

---
+++ else_&_elsif.rb
@@ -0,0 +1,16 @@
+
+puts ("Tapez un infinitif")
+
+infintif = gets.chomp
+
+if infintif == "foutre"
+  puts ("C'est un putain de gros mot")
+elsif infintif == "merdouiller"
+  puts ("watch your language, young man")
+
+else
+  puts ("C'est un verbe, quoi.")
+
+end
+
+puts ("Nous vous remercions d'avoir tapé un verbe")
